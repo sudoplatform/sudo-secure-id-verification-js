90b8cc39a0f3c84f282b61ca4eea475ff4145cf9
Sudo Platform Engineering <sudoplatform-engineering@anonyome.com>
2021-10-03 04:17:12 +0000

    Release 2.0.1

--- src/private/transformers/verifiedIdentityTransformer.ts
+++ src/private/transformers/verifiedIdentityTransformer.ts
@@ -10,16 +10,18 @@ export class VerifiedIdentityTransformer {
       owner: graphql.owner,
       verified: graphql.verified,
       verifiedAt:
-        graphql.verifiedAtEpochMs === undefined
+        graphql.verifiedAtEpochMs === undefined ||
+        graphql.verifiedAtEpochMs === null
           ? undefined
           : new Date(graphql.verifiedAtEpochMs),
       verificationMethod: VerificationMethodTransformer.toEntity(
         graphql.verificationMethod,
       ),
       canAttemptVerificationAgain: graphql.canAttemptVerificationAgain,
-      idScanUrl: graphql.idScanUrl,
+      idScanUrl: graphql.idScanUrl ?? undefined,
       requiredVerificationMethod:
-        graphql.requiredVerificationMethod === undefined
+        graphql.requiredVerificationMethod === undefined ||
+        graphql.requiredVerificationMethod === null
           ? undefined
           : VerificationMethodTransformer.toEntity(
               graphql.requiredVerificationMethod,
